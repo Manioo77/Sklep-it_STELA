4022038ba2a4da9c8cea5ac036b4658b23a1abe8
Manioo77 <mm729356@gmail.com>
2023-04-02 23:13:37 +0200

    created shopping popup, input search and mechanism with Js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const advertisementClose = document.querySelector('.advertisement__closeBtn')
 const advertisement = document.querySelector('.advertisement')
 const containerClose = document.querySelector('.container__profile--close')
+
+const shopCartClose = document.querySelector('.shopContainer__cart--close')
+const shopContainer = document.querySelector('.shopContainer')
+const shopContainerCart = document.querySelector('.shopContainer__cart')
+const purchase = document.querySelector('.fa-cart-shopping')
+
 const container = document.querySelector('.container')
 const profile = document.querySelector('.fa-user')
+const searchBtn = document.querySelector('.fa-magnifying-glass')
+const search = document.querySelector('.nav__input--search')
 //cookie
 const cookieBox = document.querySelector('.cookie__box')
 const cookieBtn = document.querySelector('.cookie__box--btn')
@@ -19,29 +27,39 @@ const showSection = () => {
 	}
 }
 
-const showPopup = () => {
-	localStorage.setItem('cookie', 'true')
+const showShopCart = () => {
+	shopContainer.classList.toggle('shopContainer--show')
+	shopContainerCart.classList.toggle('shopContainer__cart--show')
+}
 
+const showSearchInput = () => {
+	search.classList.toggle('showInputSearch')
+}
+
+const showPopup = () => {
+	document.cookie = 'cookie=true'
 	cookieBox.classList.add('hideCookie')
 }
 
-const checklocalstorage = () => {
-	const checkLocal = localStorage.getItem('cookie')
-
-	if (checkLocal) {
+const checkIsCookie = () => {
+	if (document.cookie === 'cookie=true') {
 		cookieBox.classList.add('hideCookie')
 	} else {
 		cookieBox.classList.remove('hideCookie')
 	}
 }
 
-checklocalstorage()
+checkIsCookie()
+
+searchBtn.addEventListener('click', showSearchInput)
 
 cookieBtn.addEventListener('click', showPopup)
 
-advertisementClose.addEventListener('click', e => {
-	const aside = e.target.parentElement
-	hideSection(aside)
+purchase.addEventListener('click', showShopCart)
+
+shopCartClose.addEventListener('click', () => {
+	shopContainer.classList.toggle('shopContainer--show')
+	shopContainerCart.classList.toggle('shopContainer__cart--show')
 })
 
 containerClose.addEventListener('click', e => {
@@ -50,3 +68,8 @@ containerClose.addEventListener('click', e => {
 })
 
 profile.addEventListener('click', showSection)
+
+// advertisementClose.addEventListener('click', e => {
+// 	const aside = e.target.parentElement
+// 	hideSection(aside)
+// })
